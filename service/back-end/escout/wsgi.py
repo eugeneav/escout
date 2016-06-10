62b54531dde9272c3328caa638ffa3dfbd94947a
Eugene Avrukevich <eugene.avrukevich@exposit-ds.com>
2016-06-10 18:35:46 +0300

    Update wsgi for launching under Apache 2

--- service/back-end/escout/wsgi.py
+++ service/back-end/escout/wsgi.py
@@ -6,8 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+# Import sys lib is required for apache launch
 
 import os
+import sys
+
+sys.path.append("/var/www/escout-project/escout/service/back-end")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "escout.settings")
 
 from django.core.wsgi import get_wsgi_application
